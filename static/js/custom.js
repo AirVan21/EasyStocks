c89ff44d716fe43000fb16b46cd980efa66f9a37
Ivan Abramov <airvan21@gmail.com>
2019-07-09 22:39:34 +0300

    Adds responsive logic for small screens

--- static/js/custom.js
+++ static/js/custom.js
@@ -4,10 +4,19 @@
 
     // PROJECT CAROUSEL
     $('.owl-carousel').owlCarousel({
-    	items: 3,
-	    loop:true,
-	    margin:15,
-	    nav:false
-	});
+        loop:true,
+        margin:15,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            }
+        }
+    });
 
 })(jQuery);
