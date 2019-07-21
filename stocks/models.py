4e103fd1833415d6a5054f549bf71c126c09453c
Ivan Abramov <airvan21@gmail.com>
2019-07-21 13:21:23 +0300

    Add country code to share model

--- stocks/models.py
+++ stocks/models.py
@@ -14,6 +14,7 @@ class Share(models.Model):
     title = models.CharField(max_length=200)
     ticker = models.CharField(max_length=10)
     text = models.TextField()
+    countryCode = models.CharField(max_length=3)
     dataProvider = models.ForeignKey('MarketDataProvider',
                                      on_delete=models.CASCADE,
                                      null=True,
