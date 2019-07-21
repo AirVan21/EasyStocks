cf18ad0ef32918e694ca31983cbe931c4514c31a
Ivan Abramov <airvan21@gmail.com>
2019-07-21 12:00:31 +0300

    Adds robots.txt and sitemap files

--- EasyStocks/urls.py
+++ EasyStocks/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('stocks.urls')),
-]
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt",
+         content_type='text/plain')),
+    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml",
+         content_type='text/plain')), ]
