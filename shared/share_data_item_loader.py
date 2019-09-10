a2d1f1100790f4c337289ba7fbc9ffbd08904ab0
Ivan Abramov <airvan21@gmail.com>
2019-09-10 23:34:42 +0300

    Add ShareDataItem model and related generator

--- shared/share_data_item_loader.py
+++ shared/share_data_item_loader.py
@@ -1,22 +1,38 @@
 import csv
 import pandas as pd
 import argparse
+from datetime import datetime
+from stocks.models import ShareDataItem, Share
 
 
 class ShareDataItemLoader(object):
-    def __init__(self, path_to_csv):
+    def __init__(self, path_to_csv, share_ticker):
         self.path = path_to_csv
+        self.ticker = share_ticker
 
     def load_items(self, count):
+        share_items = Share.objects.filter(ticker=self.ticker)
+        if not share_items:
+            print('Did not find related share!')
+            return
         csv_file = pd.read_csv(self.path, nrows=count, delimiter=',')
         for index, row in csv_file.iterrows():
-            # Create ShareDataItem record
-            print(index)
+            date_value = datetime.strptime(row['timestamp'], '%Y-%m-%d').date()
+            data_share_item = ShareDataItem(share=share_items.first(),
+                                            date=date_value,
+                                            open_price=row['open'],
+                                            high_price=row['high'],
+                                            low_price=row['low'],
+                                            close_price=row['close'],
+                                            volume=row['volume'])
+            data_share_item.save()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Loads stocks historical data to database.')
     parser.add_argument('path', metavar='path', type=str, help='path to csv data')
+    parser.add_argument('ticker', metavar='ticker', type=str, help='share ticker')
     args = parser.parse_args()
-    loader = ShareDataItemLoader(args.path)
+    # Loads
+    loader = ShareDataItemLoader(args.path, args.ticker)
     loader.load_items(52)
--- stocks/admin.py
+++ stocks/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Share, CurrencyInstrument, MarketDataProvider, Article
+from .models import Share, CurrencyInstrument, MarketDataProvider, Article, ShareDataItem
 
 
 admin.site.register(Share)
 admin.site.register(CurrencyInstrument)
 admin.site.register(MarketDataProvider)
 admin.site.register(Article)
+admin.site.register(ShareDataItem)
