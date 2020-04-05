783e73fb1ba20936acab6656086a9f396a34faeb
Ivan Abramov <airvan21@gmail.com>
2020-04-05 14:38:57 +0300

    Remove WTD from data service

--- stocks/tasks.py
+++ stocks/tasks.py
@@ -1,14 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import task, chain
-from shared.download_data import download_share_data_alpha, download_fx_data, download_share_data_wtd
+from shared.download_data import download_share_data_alpha, download_fx_data
 from shared.plotly_draw import generate_candle_image, generate_fx_image
 from shared.data_manager import DataManager
 from shared.alpha_download import get_aggregator_alpha
-from shared.world_trading_download import get_column_name_mapping, get_aggregator_wtd
 from shared.share_data_item_loader import ShareDataItemLoader
 from shared.news_download import get_news_api_payload, get_start_end_week_dates, download_news_json
-from shared.keys import ALPHA_DOWNLOAD_KEY, WORLD_TRADING_DATA_KEY, NEWS_API_KEY
+from shared.keys import ALPHA_DOWNLOAD_KEY, NEWS_API_KEY
 from stocks.models import Share, CurrencyInstrument, Article
 
 
@@ -26,11 +25,7 @@ def download_and_draw_share(share_name, mdp_folder, mdp_url, storage_path, img_p
         generate_candle_image(csv_path, weeks_count, img_path)
         data_item_loader.load_update()
     elif mdp_folder == 'worldtradingdata':
-        download_share_data_wtd(share_name, mdp_url, WORLD_TRADING_DATA_KEY, storage_path)
-        manager.resample_daily_data_to_weekly(get_aggregator_wtd())
-        manager.rename_columns(get_column_name_mapping())
-        generate_candle_image(csv_path, weeks_count, img_path)
-        data_item_loader.load_update()
+        raise Exception('World Trading Data service is not supported anymore.')
     else:
         logger.info('No market data provider specified!')
         return
