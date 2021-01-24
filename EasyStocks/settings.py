b611e81fa3b3ff3cc0f04ff7ad1b02f1f6bb8127
Ivan Abramov <airvan21@gmail.com>
2021-01-24 14:04:15 +0300

    Update statis data

--- EasyStocks/settings.py
+++ EasyStocks/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from celery.schedules import crontab
+from shared.keys import DJANGO_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i1ta@p%tepv9f+llz1pbpz1-%v5%pzp57s+ac=ch$iu%+@4uar'
+SECRET_KEY = DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
