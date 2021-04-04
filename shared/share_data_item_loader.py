38d2b43995c5f4cfe69d376bfb79b823c6876299
Ivan Abramov <airvan21@gmail.com>
2021-04-04 13:29:13 +0300

    Update DataShareItemLoader methods

--- shared/share_data_item_loader.py
+++ shared/share_data_item_loader.py
@@ -4,14 +4,20 @@ from stocks.models import ShareDataItem, Share
 
 
 class ShareDataItemLoader(object):
-    def __init__(self, path_to_csv, share_ticker):
-        self.path = path_to_csv
-        self.ticker = share_ticker
+    '''
+    ShareDataItemLoader class saves end-of-day data to the database
+    '''
 
-    def load_items(self, count):
-        share_items = Share.objects.filter(ticker=self.ticker)
+    def __init__(self, path_to_file):
+        self.path = path_to_file
+
+    def load_items_from_csv(self, share_ticker, count):
+        '''
+        Loads end-of-day data from the csv file to the database
+        '''
+        share_items = Share.objects.filter(ticker=share_ticker)
         if not share_items:
-            print('Did not find related share!')
+            print('Did not find related share for ticker', share_ticker)
             return
         csv_file = pd.read_csv(self.path, delimiter=',')
         csv_file['timestamp'] = pd.to_datetime(
@@ -22,8 +28,8 @@ class ShareDataItemLoader(object):
         csv_file = csv_file.head(count)
         self.load_csv(share_items.first(), csv_file)
 
-    def load_update(self):
-        share_items = Share.objects.filter(ticker=self.ticker)
+    def load_update(self, share_ticker):
+        share_items = Share.objects.filter(ticker=share_ticker)
         if not share_items:
             print('Did not find related share!')
             return
@@ -42,26 +48,34 @@ class ShareDataItemLoader(object):
             self.load_csv(selected_share, csv_file)
         else:
             last_data_item = share_data_items.first()
-            last_data_item_datetime = pd.to_datetime(last_data_item.date)
-            csv_update = csv_file[csv_file['timestamp'] > last_data_item_datetime]
+            last_datetime = pd.to_datetime(last_data_item.date)
+            csv_update = csv_file[csv_file['timestamp'] > last_datetime]
             self.load_csv(selected_share, csv_update)
 
     def load_csv(self, selected_share, csv_frame):
+        '''
+        Loads end-of-day data related to the share from the csv to database
+        '''
         for index, row in csv_frame.iterrows():
             date_value = row['timestamp'].date()
-            data_share_item = ShareDataItem(share=selected_share,
-                                            date=date_value,
-                                            open_price=row['open'],
-                                            high_price=row['high'],
-                                            low_price=row['low'],
-                                            close_price=row['close'],
-                                            volume=row['volume'])
+            data_share_item = ShareDataItem(
+                share=selected_share,
+                date=date_value,
+                open_price=row['open'],
+                high_price=row['high'],
+                low_price=row['low'],
+                close_price=row['close'],
+                volume=row['volume']
+            )
             data_share_item.save()
 
-    def clear_items(self):
-        share_items = Share.objects.filter(ticker=self.ticker)
+    def clear_items(self, share_ticker):
+        '''
+        Removes share data items related to the share from the database
+        '''
+        share_items = Share.objects.filter(ticker=share_ticker)
         if not share_items:
-            print('Did not find related share!')
+            print('Did not find related share for ticker', share_ticker)
             return
         ShareDataItem.objects.filter(share=share_items.first()).delete()
 
@@ -72,5 +86,6 @@ if __name__ == '__main__':
     parser.add_argument('ticker', metavar='ticker', type=str, help='share ticker')
     args = parser.parse_args()
     # Loads
-    loader = ShareDataItemLoader(args.path, args.ticker)
-    loader.load_items(52)
+    loader = ShareDataItemLoader(args.path)
+    number_of_weeks = 52
+    loader.load_items_from_csv(args.ticker, number_of_weeks)
